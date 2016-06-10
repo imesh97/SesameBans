3fc69126f231350e6c0d99df8c13799547d58659
imesh97 <imesh.nimsitha@gmail.com>
2016-06-10 16:23:39 -0400

    Created the main class.

---
+++ src/main/java/xyz/imdafatboss/sesamebans/Home.java
@@ -0,0 +1,15 @@
+package xyz.imdafatboss.sesamebans;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Home extends JavaPlugin implements Listener{
+
+    @Override
+    public void onEnable(){
+
+        
+
+    }
+
+}
