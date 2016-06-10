6c8801a7e6f7e628cb27f14a8ab5b36eb44ab8ba
imesh97 <imesh.nimsitha@gmail.com>
2016-06-10 16:51:51 -0400

    Initialized the FileManager.

--- src/main/java/xyz/imdafatboss/sesamebans/Home.java
+++ src/main/java/xyz/imdafatboss/sesamebans/Home.java
@@ -2,13 +2,17 @@ package xyz.imdafatboss.sesamebans;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.sesamebans.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
+
+    FileManager fm = new FileManager(this);
 
     @Override
     public void onEnable(){
 
-        
+        fm.getConfig("config.yml").saveDefaultConfig();
+        fm.getConfig("messages.yml").saveDefaultConfig();
 
     }
 
