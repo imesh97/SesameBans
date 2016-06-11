75f4fb594fd27cb029db4f4b66bd2c217e0fc09c
imesh97 <imesh.nimsitha@gmail.com>
2016-06-11 00:02:14 -0400

    Initialized the CommandManager.

--- src/main/java/xyz/imdafatboss/sesamebans/Home.java
+++ src/main/java/xyz/imdafatboss/sesamebans/Home.java
@@ -2,17 +2,23 @@ package xyz.imdafatboss.sesamebans;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.sesamebans.cmd.mgt.CommandManager;
 import xyz.imdafatboss.sesamebans.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
 
     FileManager fm = new FileManager(this);
+    CommandManager cmd;
 
     @Override
     public void onEnable(){
 
+        cmd = new CommandManager(this);
+
         fm.getConfig("config.yml").saveDefaultConfig();
         fm.getConfig("messages.yml").saveDefaultConfig();
+
+        cmd.registerCommands(this);
 
     }
 
