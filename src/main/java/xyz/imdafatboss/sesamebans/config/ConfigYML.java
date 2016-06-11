011f56c34d0e5ca0b349079bdb1f2f2f51539121
imesh97 <imesh.nimsitha@gmail.com>
2016-06-10 22:28:44 -0400

    Updated the config.yml values.

--- src/main/java/xyz/imdafatboss/sesamebans/config/ConfigYML.java
+++ src/main/java/xyz/imdafatboss/sesamebans/config/ConfigYML.java
@@ -1,5 +1,6 @@
 package xyz.imdafatboss.sesamebans.config;
 
+import org.bukkit.entity.Player;
 import xyz.imdafatboss.sesamebans.Home;
 import xyz.imdafatboss.sesamebans.utils.Msg;
 
@@ -26,9 +27,37 @@ public class ConfigYML {
 
     }
 
+    public String getStaffPermission(){
+
+        return getConfig().get().getString("perms.staff");
+
+    }
+
     public String prefix(){
 
         return getPrefix() + " ";
+
+    }
+
+    public String getDefaultKick(){
+
+        return Msg.translate(getConfig().get().getString("default.kick"));
+
+    }
+
+    public String getStaffChatFormat(){
+
+        return getConfig().get().getString("sc-format");
+
+    }
+
+    public String getStaffChat(String message, Player p){
+
+        String s = getStaffChatFormat();
+        String s1 = s.replaceAll("%player%", p.getName());
+        String s2 = s1.replaceAll("%message%", message);
+
+        return Msg.translate(s2);
 
     }
 
