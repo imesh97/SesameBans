3ec39da85d20d4ae70dbbd53c855525ed68b00a5
imesh97 <imesh.nimsitha@gmail.com>
2016-06-21 22:20:50 -0400

    Remove the tempban data after a player is unbanned.

--- src/main/java/xyz/imdafatboss/sesamebans/events/TempbanEvents.java
+++ src/main/java/xyz/imdafatboss/sesamebans/events/TempbanEvents.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import xyz.imdafatboss.sesamebans.Home;
+import xyz.imdafatboss.sesamebans.api.DataAPI;
 import xyz.imdafatboss.sesamebans.api.PlayerAPI;
 import xyz.imdafatboss.sesamebans.config.ConfigYML;
 
@@ -18,17 +19,22 @@ public class TempbanEvents implements Listener{
     }
     PlayerAPI papi;
     ConfigYML cfg;
+    DataAPI data;
 
     @EventHandler
     public void onPlayerIsTempbanned(PlayerLoginEvent e){
 
         papi = new PlayerAPI(plugin);
         cfg = new ConfigYML(plugin);
+        data = new DataAPI(plugin);
         Player p = e.getPlayer();
         if(papi.isTempbanned(p)){
 
             String banscreen = cfg.tempbanScreen(p);
             e.disallow(PlayerLoginEvent.Result.KICK_BANNED, banscreen);
+
+            data.getData().get().set("tempbans." + p.getUniqueId().toString(), null);
+            data.getData().save();
 
         }
 
